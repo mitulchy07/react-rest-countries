88ee1a5f656c9ed53d3a8a7a24302223cae95c11
mitulchy07 <mitul9616@gmail.com>
2022-09-21 00:40:20 +0200

    import done

--- rest-countries/src/App.js
+++ rest-countries/src/App.js
@@ -1,47 +1,60 @@
+import Country from './components/Country/Country';
 import './App.css';
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 
 function App() {
     return ( <
-        div className = "App" >
-        <LoadCountries></LoadCountries>
+        div className = "App" > 
+        <Country></Country>
         </div> 
     );
 }
 
-function LoadCountries(){
+
+
+
+
+
+
+
+
+
+
+
+
+// function LoadCountries(){
  
-  const [countries, setCountries] = useState([]);
-  useEffect(() =>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => setCountries(data))
+//   const [countries, setCountries] = useState([]);
+//   useEffect(() =>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res => res.json())
+//     .then(data => setCountries(data))
 
-  }, [])
-  return(
-    <div>
-      <h3> {countries.length} </h3>
-      {
-        countries.map(country => <Country name = {country.name.common} official = {country.name.official} population = {country.population} spelling = {country.altSpellings}capital = {country.capital}>
+//   }, [])
+//   return(
+//     <div>
+//       <h3> {countries.length} </h3>
+//       {
+//         countries.map(country => <Country name = {country.name.common} official = {country.name.official} population = {country.population} spelling = {country.altSpellings}capital = {country.capital}>
           
-        </Country>)
-      }
-    </div>
-  )
-}
+//         </Country>)
+//       }
+//     </div>
+//   )
+// }
 
-function Country(props){
-  return(
-    <div className='container'>
-      <h2>Name: {props.name} </h2>
-      <h3>Capital: {props.capital} </h3>
-      <h4>Official name: {props.official} </h4>
-      <p>Population: {props.population} </p>
-      <p>Old Spellings: {props.spelling} </p>
+// function Country(props){
+//   return(
+//     <div className='container'>
+//       <h2>Name: {props.name} </h2>
+//       <h3>Capital: {props.capital} </h3>
+//       <h4>Official name: {props.official} </h4>
+//       <p>Population: {props.population} </p>
+//       <p>Old Spellings: {props.spelling} </p>
 
-    </div>
-  )
+//     </div>
+//   )
 
-}
+// }
 export default App;
---
+++ rest-countries/src/components/Country/Country.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Country.css'
+
+const Country = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+export default Country;
